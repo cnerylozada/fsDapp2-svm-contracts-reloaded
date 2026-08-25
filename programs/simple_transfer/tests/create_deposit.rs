f56ba917c0e7d1f12b3a059112b380fe82ef32f4
cnerylozada <cnerylozada@gmail.com>
2026-08-25 15:22:40 -0500

    feat: adding aditional test

--- programs/simple_transfer/tests/create_deposit.rs
+++ programs/simple_transfer/tests/create_deposit.rs
@@ -1,3 +1,4 @@
+use anchor_lang::system_program;
 use borsh::BorshDeserialize;
 use solana_sdk::signer::Signer;
 
@@ -28,7 +29,9 @@ fn create_deposit() {
     assert_eq!(create_deposit_tx_result.is_ok(), true);
 
     let deposit_account_raw = svm.get_account(&deposit_account_pda).unwrap();
+    let vault_account_raw = svm.get_account(&vault_account_pda).unwrap();
     assert_eq!(deposit_account_raw.owner, simple_transfer::ID);
+    assert_eq!(vault_account_raw.owner, system_program::ID);
 
     let deposit_account = DepositAccount::deserialize(&mut &deposit_account_raw.data[8..]).unwrap();
     assert_eq!(deposit_account.owner, signer.pubkey());
